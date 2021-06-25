5babdfea087e4ba07cf781f2cfe905f0071c7181
andrey373 <andreyinfo737@gmail.com>
2021-06-25 19:09:26 +0300

    added scrollbar

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ form.addEventListener('submit', function(evt){
         list.appendChild(createItem(form.textTask.value));
         removeElement();
         doneElement();
+        sclInit();
         form.textTask.value = '';
     }
 })
@@ -41,21 +42,101 @@ function doneElement (){
     btnDone.forEach(element => {
         element.onclick = () => {
             if(element.parentElement.previousElementSibling.querySelector('input[type="checkbox"]').checked){
-                let textForItemDone = element.parentElement.previousElementSibling.querySelector('.item_text').innerText;
-                let doneItem = taskDone.cloneNode(false);
-                    doneItem.textContent = textForItemDone;
-        
-                    listDone.appendChild(doneItem);
-                    element.parentElement.parentElement.remove();
+                list.querySelectorAll('.task_item').forEach((elt) => {
+                    if(elt.querySelector('input[type="checkbox"]').checked){
+                        let textForItemDone = elt.querySelector('.item_text').innerText;
+                        let doneItem = taskDone.cloneNode(false);
+                        doneItem.textContent = textForItemDone;
+            
+                        listDone.appendChild(doneItem);
+                        elt.remove();
+                    }
+                });
+                sclInit();
             }
         }
+        
     });
 }
 doneElement();
 
 cleanTask.onclick = function(){
     list.innerHTML = '';
+    sclInit();
 }
 cleanDone.onclick = function(){
     listDone.innerHTML = '';
+    sclInit();
 }
+
+
+function sclInit() {
+
+	class ScrollBox {
+		static SCROLLER_HEIGHT_MIN = 25;
+
+		constructor(container) {
+			this.viewport = container.querySelector('.viewport');
+			this.contentBox = container.querySelector('.content-box');
+			this.contentList = container.querySelector('.content-list');
+            this.scrollbar = container.querySelector('.scrollbar');
+			this.pressed = false;
+			this.init();
+		}
+
+		init() {
+			this.viewportHeight = this.viewport.offsetHeight;
+			this.contentHeight = this.contentList.scrollHeight;
+			if (this.viewportHeight >= this.contentHeight){
+                this.scrollbar.classList.remove('scrollbar--active');
+                return;
+            };
+			this.max = this.viewport.clientHeight - this.contentHeight;
+			this.ratio = this.viewportHeight / this.contentHeight;
+			this.createScrollbar();
+			this.registerEventsHandler();
+		}
+
+		createScrollbar() {
+			this.scrollbar.classList.add('scrollbar--active');
+			this.scroller = this.viewport.querySelector('.scroller');
+			this.scrollerHeight = parseInt(this.ratio * this.viewportHeight);
+			this.scrollerHeight = (this.scrollerHeight <= ScrollBox.SCROLLER_HEIGHT_MIN) ? ScrollBox.SCROLLER_HEIGHT_MIN : this.scrollerHeight;
+			this.scroller.style.height = this.scrollerHeight + 'px';
+		}
+
+		registerEventsHandler(e) {
+			this.contentBox.addEventListener('scroll', () => {
+				this.scroller.style.top = (this.contentBox.scrollTop * this.ratio) + 'px';
+			});
+
+			this.scroller.addEventListener('mousedown', e => {
+				this.start = e.clientY;
+				this.pressed = true;
+			});
+
+			document.addEventListener('mousemove', this.drop.bind(this));
+			document.addEventListener('mouseup', () => this.pressed = false);
+		}
+
+		drop(e) {
+			e.preventDefault();
+			if (this.pressed === false) return;
+			let shiftScroller = this.start - e.clientY;
+			this.scroller.style.top = (this.scroller.offsetTop - shiftScroller) + 'px';
+			let shiftContent = this.scroller.offsetTop / this.ratio;
+			const totalheightScroller = this.scroller.offsetHeight + this.scroller.offsetTop;
+			const maxOffsetScroller = this.viewportHeight - this.scroller.offsetHeight;
+			if (this.scroller.offsetTop < 0) this.scroller.style.top = '0px';
+			if (totalheightScroller >= this.viewportHeight) this.scroller.style.top = maxOffsetScroller + 'px';
+			this.contentBox.scrollTo(0, shiftContent);
+			this.start = e.clientY;
+		}
+	}
+
+	const containers = document.querySelectorAll('.container');
+	for (let container of containers) {
+		const scrollbox = new ScrollBox(container);
+	}
+};
+sclInit();
